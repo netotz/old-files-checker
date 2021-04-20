66fd12c742f8d43a1cc67f0c4a7aa61627eff7f3
netotz <neto.otz@hotmail.com>
2021-04-20 11:10:27 -0500

    Add method to delete old folders

--- OldFilesChecker/Program.cs
+++ OldFilesChecker/Program.cs
@@ -4,34 +4,63 @@ using System.Linq;
 
 namespace OldFilesChecker {
 
-    class Program {
+    public class Program {
 
         /// <summary>
-        /// Deletes the oldest <paramref name="oldFiles"/> number of files of extension <paramref name="extension"/>
-        /// if there are at least <paramref name="minimumFiles"/> number of files in the directory with path <paramref name="folderPath"/>.
+        /// Deletes the oldest number of folders older than some days, or files of a certain extension
+        /// if there are at least some number of files in the specified directory with path.
         /// </summary>
-        /// <param name="folderPath">Path of the directory that contains the files to check.</param>
-        /// <param name="minimumFiles">Minimum number of files that must exist in the directory.</param>
-        /// <param name="oldFiles">Number of files to delete.</param>
-        /// <param name="extension">
-        /// Extension of the files to check, e.g. <c>txt</c>.
-        /// If it's not specified then all the files will be checked.
-        /// </param>
-        static void Main(string folderPath, int minimumFiles, int oldFiles, string extension = "*") {
-            var files = new DirectoryInfo(folderPath)
+        /// <param name="directoryPath">Path of the directory that contains the files to check</param>
+        /// <param name="minimumFiles">Minimum number of files that must exist in the directory</param>
+        /// <param name="oldFiles">Number of files to delete</param>
+        /// <param name="daysOld">Delete all folders older than this number of days. If specified, then
+        /// <paramref name="minimumFiles"/> and <paramref name="oldFiles"/> options are ignored</param>
+        /// <param name="extension">Extension of the files to check, e.g. <c>txt</c>
+        /// If it's not specified then all the files will be checked.</param>
+        public static void Main(string directoryPath, int minimumFiles, int oldFiles, int daysOld = 0, string extension = "*") {
+            var directory = new DirectoryInfo(directoryPath);
+
+            if (daysOld > 0) {
+                DeleteFolders(directory, daysOld);
+            }
+            else {
+                DeleteFiles(directory, minimumFiles, oldFiles, extension);
+            }
+        }
+
+        public static void DeleteFiles(DirectoryInfo directory, int minimum, int old, string extension) {
+            var files = directory
                 .GetFiles($"*.{extension}")
                 .OrderBy(f => f.LastWriteTime)
                 .ToArray();
 
-            if (files.Length < minimumFiles) {
-                Console.WriteLine($"The directory {folderPath} only contains {files.Length} files. " +
+            if (files.Length < minimum) {
+                Console.WriteLine($"The directory {directory.FullName} only contains {files.Length} files. " +
                     "Verify the extension and minimum files arguments.");
                 return;
             }
 
-            foreach (var file in files[..oldFiles]) {
+            foreach (var file in files[..old]) {
                 file.Delete();
+                Console.WriteLine($"Deleted file '{file.Name}'.");
             }
+
+
+        }
+
+        public static void DeleteFolders(DirectoryInfo directory, int daysOld) {
+            var folders = directory
+                .GetDirectories();
+
+            var oldestFolders = folders
+                .Where(f => (DateTime.Now - f.LastWriteTime).TotalDays >= daysOld)
+                .ToArray();
+            foreach (var folder in oldestFolders) {
+                folder.Delete();
+                Console.WriteLine($"Deleted folder '{folder.Name}' ({folder.LastWriteTime}).");
+            }
+
+            Console.WriteLine($"Total deleted folders: {oldestFolders.Length}");
         }
     }
 }
